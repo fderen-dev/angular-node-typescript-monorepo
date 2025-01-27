47a225e0af47d8f9ae19e2f38812145d0b7c21f6
fderen-dev <fderen.dev@gmail.com>
2025-01-27 18:55:39 +0100

    Updated Angular

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -2,15 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'exp-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  template: `
+    selector: 'exp-root',
+    imports: [RouterOutlet],
+    template: `
     <h1>Welcome to {{ title }}!</h1>
 
     <router-outlet />
   `,
-  styleUrl: './app.component.scss',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = '@expenses-tracker/web';
